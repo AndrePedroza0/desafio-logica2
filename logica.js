118421427905264fe97dad14a3ad17dd5a892061
AndrePedroza0 <72953683+AndrePedroza0@users.noreply.github.com>
2024-02-22 14:44:11 -0300

    Add files via upload
    Desafio

---
+++ logica.js
@@ -0,0 +1,26 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoDeVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias > 20 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias > 50 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias > 80 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias > 90 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoDeVitorias, nivel };
+}
+
+
+const resultado = calcularNivel(90, 20);
+console.log("O Herói tem um saldo de " + resultado.saldoDeVitorias + " está no nível de " + resultado.nivel);
